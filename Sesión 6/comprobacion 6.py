4d13c3c6fb09f4944dc933019204258256aeecc3
Tu nombre de usuario en GitHub <Tu direccin de correo electrnico en GitHub>
2023-04-21 22:18:43 -0400

    Cambios en la actividad

--- Sesión 6/comprobacion 6.py
+++ Sesión 6/comprobacion 6.py
@@ -1,12 +1,12 @@
 def analisis_numero(n):
     if n >= 0:
-     if n == 0:
-       return("El número es 0")
-     else:
-      return("El número es positivo")
+        if n == 0:
+            return("El número es 0")
+        else:
+            return("El número es positivo")
   
     else:
-     return("El número es negativo")
+        return("El número es negativo")
 
 n = int(input("Ingrese un número "))
 print(analisis_numero(n))
