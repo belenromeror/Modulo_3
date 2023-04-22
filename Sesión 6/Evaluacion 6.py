e5e65a2df1d26a23498f2275185f08ab06027bf4
Tu nombre de usuario en GitHub <Tu direccin de correo electrnico en GitHub>
2023-04-21 21:56:42 -0400

    Sesión 6

---
+++ Sesión 6/Evaluacion 6.py
@@ -0,0 +1,28 @@
+def orden_numeros(n1,n2,n3):
+  if n1 > n2 and n1 > n3:
+        if n2 > n3:
+            return (n1, n2, n3)
+        else:
+            return (n1, n3, n2)
+  elif n2 > n1 and n2 > n3:
+        if n1 > n3:
+            return (n2, n1, n3)
+        else:
+            return (n2, n3, n1)
+  else:
+        if n1 > n2:
+            return (n3, n1, n2)
+        else:
+            return (n3, n2, n1)
+
+n1 = int(input("Ingrese un número "))
+n2 = int(input("Ingrese un número "))
+n3 = int(input("Ingrese un número "))
+print(orden_numeros(n1,n2,n3))
+    
+    
+
+   
+        
+ 
+
