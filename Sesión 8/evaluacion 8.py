efb8848eacb0c9baf9979aeae24ada495205ed0f
Tu nombre de usuario en GitHub <Tu direccin de correo electrnico en GitHub>
2023-04-28 12:59:11 -0400

    cambio actividad 8

--- Sesión 8/evaluacion 8.py
+++ Sesión 8/evaluacion 8.py
@@ -1,15 +1,20 @@
-#Lista entregada
-lista = [[1,2,3], [0,4,5], [4,0,1], [6,5,4]]
-diccionario = {}
-for i, sublista in enumerate(lista):
-    if sublista[0] == 0:
-        continue
-    subdiccionario = {}
-    for j, elemento in enumerate (sublista):
-        if elemento == 0:
-            continue
-        subdiccionario [f"{chr(ord('A')+i)}{j+1}"] = elemento
-    diccionario[chr(ord('A')+i)] = subdiccionario
 
-for  subdiccionario in diccionario.items():
-    print(subdiccionario)
+lista = [[1,2,3], [0,4,5], [4,0,1], [6,5,4]]
+
+# Crear un diccionario vacío
+result_dict = {}
+
+# Recorrer cada sublista y cada elemento dentro de ellas
+for i in range(len(lista)):
+    # Si el primer número es cero, omitir la sublista
+    if lista[i][0] == 0:
+        continue
+    # Si hay un cero en cualquier otra posición, omitir solo la impresión del cero
+    for j in range(len(lista[i])):
+        if lista[i][j] == 0:
+            continue
+        # Asignar cada sublista al diccionario correspondiente
+        result_dict[chr(i+65)] = lista[i]
+
+# Imprimir el diccionario resultante
+print(result_dict)
