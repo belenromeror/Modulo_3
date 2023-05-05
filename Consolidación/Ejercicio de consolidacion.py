08504d49a7570c7a53904a469a90dc5491d9b6ba
Tu nombre de usuario en GitHub <Tu direccin de correo electrnico en GitHub>
2023-05-04 20:50:38 -0400

    Consolidación de la unidad

--- Consolidación/Ejercicio de consolidacion.py
+++ Consolidación/Ejercicio de consolidacion.py
@@ -1,3 +1,7 @@
+#Definir constante
+MAGOS = {"Harry Houdini", "David Blaine", "Teller"}
+CIENTIFICOS = {"Newton","Hawking","Einstein"}
+
 #definir categorias
 magos = []
 científicos = []
@@ -7,24 +11,39 @@ otros = []
 nombres = ["Harry Houdini","Newton", "David Blaine", "Hawking", "Messi", "Teller", "Einstein", "Pele", "Juanes"]
 
 for nombre in nombres:
-    if nombre in ["Harry Houdini", "David Blaine", "Teller"]:
+    if nombre in MAGOS:
         magos.append(nombre)
-    elif nombre in ["Newton","Hawking","Einstein"]:
+    elif nombre in CIENTIFICOS:
         científicos.append(nombre)
     else:
         otros.append(nombre)
 
 def hacer_grandioso(magos):
     for i in range(len(magos)):
-        magos[i] = magos[i].replace(magos[i], "El gran " + magos[i])
+        magos[i] = ("El gran " + magos[i])
 
-
+def imprimir_nombres(lista):
+    for elementos in lista:
+        print(elementos)
 
 #Imprimir todos los nombres
-print(nombres)
+print("Todos los nombres")
+imprimir_nombres(nombres)
 
-#Imprimir magos
-print("Nombre de los magos grandiosos"+str(magos))
+#Imprimir los nombres de los magos
+hacer_grandioso(magos)
+print("Nombre de los magos grandiosos")
+imprimir_nombres(magos)
+
+#Imprimir los nombres de los cientificos
+print("Nombre de los cientificos")
+imprimir_nombres(científicos)
+
+#Imprimir los nombres de los otros
+print("Nombres restantes")
+imprimir_nombres(otros)
 
 
- 
+
+
+
